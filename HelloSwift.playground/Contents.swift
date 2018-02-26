87aca53aff8eab031c59db1beaffc34dd4d40d11
Zhang Yu <351045731@qq.com>
2018-02-26 14:14:32 +0800

    HelloSwif提交

---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+let a = 1 + 2
+
+for x in 0...10 {
+    print("\(x) ", terminator: "")
+}
+print()
+
+
